a749ce33ee1f22606213eb39b5a639b0f0695bbd
Amit Saxena <amit1235813@gmail.com>
2020-06-15 04:42:25 +0530

    Refactored the Get User javascript using Object oriented programming

--- frontend/scripts/users/user-list-api.js
+++ frontend/scripts/users/user-list-api.js
@@ -1,39 +1,44 @@
-import {addDOMElements} from './user-list-dom-elements.js';
+import { addUserList } from "./user-list-dom-elements.js";
+
+export let modifyUserReq = {};
 
 // GET ALL - Gets user list.
 //Alows to delete a user
 //Allows to click user name to see user details
 //Runs when URL is loaded
-export function getUserReq() {
+modifyUserReq.getUserReq = function () {
   // let isAuthenticated = localStorage.getItem('isAuthenticated');
-  console.log('get users req initiated');
+  console.log("get users req initiated");
   let token = document.cookie;
-  console.log('Token sent to receive users list :', token, document.cookie);
+  console.log("Token sent to receive users list :", token, document.cookie);
   let xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-      if (this.status === 200 && this.readyState === 4) {
-        let protectedDiv = document.getElementById('protected');
-        protectedDiv.style.display = 'block';
-        let resString = this.responseText;
-        let resArray = JSON.parse(resString);
-        console.log('view user - type of res', typeof(res));
-        console.log('response status 200', this.status, this.readyState);
-        //console.log(this.responseText);
-        //detectUserList(res);
-        //const mainDiv = document.getElementById('main');
-        addDOMElements(resArray);
-        //mainDiv.innerHTML = resArray;
-      
-      } else if (this.readyState !== 4) {
-        //We do not want to tell user what error exactly - otherwise a malicious user can misuse
-        // console.log(this.responseText);
-        console.log('response status not 200', this.status, this.statusText, this.readyState);
-        
-      } else if (this.status !== 200) {
-        console.log('response status not 200', this.status, this.readyState);
-        location.href = "../index.html";
-      }
-      
+  xhttp.onreadystatechange = function () {
+    if (this.status === 200 && this.readyState === 4) {
+      let protectedDiv = document.getElementById("protected");
+      protectedDiv.style.display = "block";
+      let resString = this.responseText;
+      let resArray = JSON.parse(resString);
+      console.log("view user - type of res", typeof (res));
+      console.log("response status 200", this.status, this.readyState);
+      //console.log(this.responseText);
+      //detectUserList(res);
+      //const mainDiv = document.getElementById('main');
+      addUserList.resArray = resArray;
+      addUserList.addDOMElements();
+      //mainDiv.innerHTML = resArray;
+    } else if (this.readyState !== 4) {
+      //We do not want to tell user what error exactly - otherwise a malicious user can misuse
+      // console.log(this.responseText);
+      console.log(
+        "response status not 200",
+        this.status,
+        this.statusText,
+        this.readyState,
+      );
+    } else if (this.status !== 200) {
+      console.log("response status not 200", this.status, this.readyState);
+      location.href = "../index.html";
+    }
   };
 
   xhttp.open("GET", "api?m=" + Math.random(), true);
@@ -43,29 +48,32 @@ export function getUserReq() {
 
 //DELETE
 //Can delete user with the delete button
-export function deleteUserReq(queryParams) {
-    console.log('delete user req initiated');
-    let xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.status == 200 && this.readyState == 4) {
-          let resString = this.responseText;
-          let resArray = JSON.parse(resString);
-          console.log('view user - type of res', typeof(res), resArray);
-          console.log('response status 200', this.status, this.readyState);
-          alert('Details of Team Mate successfully deleted. Moving back to list of Team Mates');
-          location.href = 'users-list.html';
-        } else if (this.readyState !== 4) {
-          //We do not want to tell user what error exactly - otherwise a malicious user can misuse
-          //console.log(this.responseText);
-          console.log('response status not 200', this.status, this.readyState);
-          
-        } else if (this.status !== 200) {
-          alert('User details could not be deleted');
-        }
-    };
-  
-    xhttp.open("DELETE", "api/" + queryParams, true);
-    xhttp.setRequestHeader("Content-type", "application/json");
-    xhttp.send();
-  }
-  
+
+modifyUserReq.queryParams = undefined;
+
+modifyUserReq.deleteUserReq = function () {
+  console.log("delete user req initiated");
+  let xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.status == 200 && this.readyState == 4) {
+      let resString = this.responseText;
+      let resArray = JSON.parse(resString);
+      console.log("view user - type of res", typeof (res), resArray);
+      console.log("response status 200", this.status, this.readyState);
+      alert(
+        "Details of Team Mate successfully deleted. Moving back to list of Team Mates",
+      );
+      location.href = "users-list.html";
+    } else if (this.readyState !== 4) {
+      //We do not want to tell user what error exactly - otherwise a malicious user can misuse
+      //console.log(this.responseText);
+      console.log("response status not 200", this.status, this.readyState);
+    } else if (this.status !== 200) {
+      alert("User details could not be deleted");
+    }
+  };
+
+  xhttp.open("DELETE", "api/" + this.queryParams, true);
+  xhttp.setRequestHeader("Content-type", "application/json");
+  xhttp.send();
+};
--- frontend/scripts/users/user-list-event-listeners.js
+++ frontend/scripts/users/user-list-event-listeners.js
@@ -1,62 +1,63 @@
-import {getUserReq, deleteUserReq} from './user-list-api.js';
+import { modifyUserReq } from "./user-list-api.js";
 
 //On load, display user list
-window.addEventListener('load', getUserReq);
+window.addEventListener("load", modifyUserReq.getUserReq);
 
-
-
-//Change URL when user name is clicked
-//The paramters created will be used in an editing user request
-export let textArray;
-
-let mainDiv = document.getElementById('main');
-mainDiv.addEventListener("click", function(event) {
-  console.log('user list div clicked', event.target);
-  if (event.target && event.target.className === 'user-list-item') {
+let modifyUser = {};
+// Move to the user details page
+modifyUser.showUserDetails = function (event) {
+  //Change URL when user name is clicked
+  //The paramters created will be used in an editing user request
+  let textArray;
+  console.log("user list div clicked", event.target);
+  if (event.target && event.target.className === "user-list-item") {
     event.preventDefault();
-    console.log('on user name click', event.target);
+    console.log("on user name click", event.target);
     //let href = event.target.getAttribute("href");
     //console.log('user details original href', href);
     let textContent = event.target.textContent;
-    textArray = textContent.split(' ');
+    textArray = textContent.split(" ");
 
-    console.log('user details href', location.href);
-    location.href = 'user-details.html' + '?p1=' + textArray[0] + '&p2=' + textArray[1];
-    //change href
-    //location.href = 'user-details.html';
-    //pass data to next file to send as object - reload wont have data??
-    //window.open(location.href);
-
+    console.log("user details href", location.href);
+    location.href = "user-details.html" + "?p1=" + textArray[0] + "&p2=" +
+      textArray[1];
   }
+};
 
-})
-
-//Create parameters used to send a delete user request
-mainDiv.addEventListener("click", function(event) {
-  if (event.target && event.target.className === 'delete-user-button') {
-    console.log('user name clicked', event.target);
-    console.log('on delete button click', event.target);
+// Delete a user
+modifyUser.deleteUser = function (event) {
+  if (event.target && event.target.className === "delete-user-button") {
+    console.log("user name clicked", event.target);
+    console.log("on delete button click", event.target);
     //https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_confirm
-    confirm('All details of Team Mate will be deleted. Would you like to contunue?');
+    confirm(
+      "All details of Team Mate will be deleted. Would you like to contunue?",
+    );
     let textContent = event.target.previousSibling.firstChild.textContent;
-    textArray = textContent.split(' ');
-    let queryParams = 'user?p1=' + textArray[0] + '&p2=' + textArray[1];
-    deleteUserReq(queryParams);
+    textArray = textContent.split(" ");
+    //Create parameters used to send a delete user request
+    let queryParams = "user?p1=" + textArray[0] + "&p2=" + textArray[1];
+    modifyUserReq.queryParams = queryParams;
+    modifyUserReq.deleteUserReq();
   }
-
-})
+};
 
 //When create bill group button is clicked
-mainDiv.addEventListener("click", function(event) {
-  if (event.target && event.target.className === 'create-bill-group-button') {
-    console.log('user name clicked', event.target);
-    console.log('on delete button click', event.target);
-    //https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_confirm
-    confirm('All details of Team Mate will be deleted. Would you like to contunue?');
-    let textContent = event.target.previousSibling.firstChild.textContent;
-    textArray = textContent.split(' ');
-    let queryParams = 'user?p1=' + textArray[0] + '&p2=' + textArray[1];
-    deleteUserReq(queryParams);
-  }
+// mainDiv.addEventListener("click", function(event) {
+//   if (event.target && event.target.className === 'create-bill-group-button') {
+//     console.log('user name clicked', event.target);
+//     console.log('on delete button click', event.target);
+//     //https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_confirm
+//     confirm('All details of Team Mate will be deleted. Would you like to contunue?');
+//     let textContent = event.target.previousSibling.firstChild.textContent;
+//     textArray = textContent.split(' ');
+//     let queryParams = 'user?p1=' + textArray[0] + '&p2=' + textArray[1];
+//     deleteUserReq(queryParams);
+//   }
 
-})
+// })
+
+//Event listeners
+let mainDiv = document.getElementById("main");
+mainDiv.addEventListener("click", modifyUser.showUserDetails);
+mainDiv.addEventListener("click", modifyUser.deleteUser);
